bdbac7962be3e356602dbab95b3dc02e56a18dfd
Slayer <slayerpro27@gmail.com>
2024-10-03 16:48:14 +0300

    Small refactoring with small improvements

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -1,6 +1,21 @@
 import re
 
 
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'ru-RU,ru;q=0.9',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/json',
+    'Origin': 'https://telegram.blum.codes',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'Sec-Ch-Ua-Mobile': '?1',
+    'Sec-Ch-Ua-Platform': 'Android',
+    'X-Requested-With': 'org.telegram.messenger',
+}
+
+
 def get_sec_ch_ua(user_agent):
     pattern = r'(Chrome|Chromium)\/(\d+)\.(\d+)\.(\d+)\.(\d+)'
 
@@ -18,19 +33,3 @@ def get_sec_ch_ua(user_agent):
         return {'Sec-Ch-Ua': sec_ch_ua}
     else:
         return {}
-
-
-headers = {
-    'Accept': 'application/json, text/plain, */*',
-    'Accept-Language': 'ru-RU,ru;q=0.9',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/json',
-    'Origin': 'https://telegram.blum.codes',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-site',
-    'Sec-Ch-Ua': '"Google Chrome";v="127", "Chromium";v="127", "Not.A/Brand";v="24"',
-    'Sec-Ch-Ua-Mobile': '?1',
-    'Sec-Ch-Ua-Platform': 'Android',
-    'X-Requested-With': 'org.telegram.messenger',
-}
